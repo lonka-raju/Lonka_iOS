bfc665641b238b8eed6e0f42cc09a65f364b9b11
raju_lonka <raju@quadrantsystems.com>
2020-01-03 18:28:34 +0530

    Initial Commit

---
+++ Exisit-Remote-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exisit-Remote-iOS
+//
+//  Created by Quadrant Apple on 1/3/20.
+//  Copyright © 2020 Quadrant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
