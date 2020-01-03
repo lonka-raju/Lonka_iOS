e84ff896f00d84df1ab6d58677902a2716e94850
raju_lonka <raju@quadrantsystems.com>
2020-01-03 20:18:38 +0530

    added

--- Exisit-Remote-iOS/AppDelegate.swift
+++ Exisit-Remote-iOS/AppDelegate.swift
@@ -10,12 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-      print("exist remote code pushed.")
+         print("exist remote code pushed.")
          print("merge-request")
         return true
     }
